5403e187a53c21a1fc2243589b37c23e39663326
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2023-01-11 20:41:12 -0400

    promises

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const file1 = require("./file1");
 const file2 = require("./file2");
 const fs = require("fs");
-console.log("1");
-fs.readFile("./sample.txt", "utf-8", (err, data) => {
-    console.log("3");
-    if (err) {
-        console.log("errorrrrr");
-    } else {
-        console.log(data)
-    }
-});
+const util = require("util");
 
-console.log("2");
+
+
+(async () => {
+    const readFilePromise = util.promisify(fs.readFile);
+    console.log("1");
+    const data = await readFilePromise("./sample.txt", "utf-8");
+    console.log(data);
+    console.log("2");
+
+})();
+
+
+
