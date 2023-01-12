18fcede1d686d065646798685fcd62d604b5e73e
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2023-01-11 21:34:36 -0400

    mi primera peticion express

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const cuadrado = require("./file1");
+const express = require("express");
+const app = express();
 
-let obj = {
-    num: 10,
-};
-cuadrado(obj);
+app.get("/api/test", (req, res) => {
+    res.status(200).json({
+        peticion: "GET",
+        ruta: "/api/test"
+    });
+});
 
+app.listen(process.env.PORT, () => {
+    console.log(`App running on port ${process.env.PORT}`);
+})
 
-console.log(obj);//100
 
 
 
