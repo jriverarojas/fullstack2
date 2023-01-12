85972dab740c8d7667dfdc5f0d027e61431a968a
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2023-01-11 20:22:28 -0400

    callback

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const file1 = require("./file1");
 const file2 = require("./file2");
+const fs = require("fs");
+console.log("1");
+fs.readFile("./sample.txt", "utf-8", (err, data) => {
+    console.log("3");
+    if (err) {
+        console.log("errorrrrr");
+    } else {
+        console.log(data)
+    }
+});
 
-
-file1();
-console.log(file2);
+console.log("2");
