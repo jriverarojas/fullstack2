ea4d3096c8b8f3c9df7b618f6265b19342099678
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2023-01-11 22:04:44 -0400

    middlewares

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    //guardar en los logs la peticion
+    next();
+});
 
 app.get("/api/test", (req, res) => {
+    console.log(req.body);
+    res.status(200).json({
+        peticion: "GET",
+        ruta: "/api/test"
+    });
+});
+
+app.get("/api/test2", (req, res) => {
+    console.log(req.body);
     res.status(200).json({
         peticion: "GET",
         ruta: "/api/test"
