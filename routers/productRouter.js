0b742d88955975e1232a0a39e7a8f25a19775e30
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2023-01-12 21:15:23 -0400

    adding router

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -3,18 +3,44 @@ const productRouter = express.Router();
 
 productRouter
     .route("/")
-    .get((req, res, next) => {
+    .get((req, res) => {
         res.status(200).json({
             status: "ok",
             action: "get all products"
-        })
+        });
      })
-     .post((req, res, next) => {
-         console.log(req.body);
+     .post((req, res) => {
          res.status(200).json({
             status: "ok",
-            action: "post a product"
+            action: "post a product",
+            params: req.body
         })
      });
+productRouter
+    .route("/:id")
+    .get((req, res) => {
+        res.status(200).json({
+            status: "ok",
+            action: "get a product",
+            id: req.params.id
+        })
+
+    })
+    .put((req, res) => {
+        res.status(200).json({
+            status: "ok",
+            action: "modify a product",
+            id: req.params.id
+        })
+
+    })
+    .delete((req, res) => {
+        res.status(200).json({
+            status: "ok",
+            action: "delete a product",
+            id: req.params.id
+        })
+
+    })
 
 module.exports = productRouter;
